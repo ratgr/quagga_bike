cf92021b11a887526131147e044152990215f010
Ricardo <ratgricardo@gmail.com>
2017-04-24 18:27:00 -0500

    removing temporals from migrate

--- tools/migrate.php
+++ tools/migrate.php
@@ -6,6 +6,7 @@ foreach (glob(__DIR__ . "/../migrations/*.php") as $filename)
 {
     //print PHP_EOL;
     print PHP_EOL;
-    print $filename .PHP_EOL;
-    (require_once $filename)->migrate();
+    print $filename . PHP_EOL;
+    $temp = require $filename;
+    $temp->migrate();
 }
