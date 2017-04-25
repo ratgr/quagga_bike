f3b60d1c796c551bc24ad608280be87a8e10091f
Ricardo <ratgricardo@gmail.com>
2017-04-24 19:51:39 -0500

    avoiding parse

--- embeded/gps.php
+++ embeded/gps.php
@@ -28,6 +28,7 @@ $response = function() use ($INPUT){
 echo json_encode($response());
 
 function ParseGPS($id, $gps){
+    return true;
     try {
         $array = explode($gps,",");
     //create the date
