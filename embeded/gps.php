64c002e600eb94209633b30ffbe8e6808ed181f1
Ricardo <ratgricardo@gmail.com>
2017-04-24 21:20:59 -0500

    prueba fecha 2

--- embeded/gps.php
+++ embeded/gps.php
@@ -33,7 +33,7 @@ function ParseGPS($id, $gps){
     //create the date
     if(count($array) < 10) return ["error"=> true, "error_msg" => "Bad GPS RMC code", "array_count" => count($array), "gpscode" => $gps ];
     if($array[9] == "" ||$array[3] == "" || $array[5] == "" )  return ["error"=> true,  "error_msg" => "Empty RMC code" ];
-     echo "si tiene datos ";
+    echo "si tiene datos ";
     $date = DateTime::createFromFormat("dmyHis",$array[9] . $array[1])->format(DateTime::ATOM);
     echo "recupere fecha ";
     $lat = ($array[4]=="N" ? 1:-1) * (float)ltrim($array[3],"0");
